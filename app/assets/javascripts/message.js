35704c0de3e695e8d9e7c90e3c05ea7bd78896a7
ynskmt <yrtiktwyr.0427@gmail.com>
2019-11-20 01:34:18 +0900

    add ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,5 +3,13 @@ $(function(){
     e.preventDefault();
     let formData = new FormData(this);
     let url = $(this).attr("action");
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   })
 })
